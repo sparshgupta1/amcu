3d30c432d5743ae930a9ccdf5d0631231a18055a
Sparsh Gupta <sparsh.gupta@hotmail.com>
2024-10-21 21:22:30 -0700

    Update xrobj_hdr_defs.h

--- include/xrobj_hdr_defs.h
+++ include/xrobj_hdr_defs.h
@@ -20,71 +20,6 @@
 
 #include <stdint.h>
 
-#define OBJNAME_MAX_SIZE 20
-
-/*
- * Index limits
- */
-#define INDEX_ALL_DIFFDATA 0xAB7C
-#define ALIGN_STRUCT (2)
-typedef enum
-{
-    WARM,
-    LKG,
-    ARCH_CNT
-} ArchiveIndex;
-
-#define WARM_ARCHIVE_NAME "WARM"
-#define LKG_ARCHIVE_NAME "LKG"
-
-#define CALRECORDHEADERLEN 8
-/*CALBRD_NAME needs to be exactly CALRECORDHEADERLEN(8) characters long*/
-#define CALBRD_NAME "INFNCALB"
-
-/*The valid object name is based off ascii characters limited to
- * [0(0x30)-9(0x39)], [A(0x41)-Z(0x5A)] & _(0x5F). We use this magic number and
- * substract this against the object name. */
-#define CAL_ENCRYPT_MAGIC_NUM (0x2F)
-
-/*Calibration version where the data is unencrypted and peeking into the raw
- * binary doesn't yield any meaningful ASCII value*/
-#define CAL_UNECRYTPTED_VERSION (1)
-
-#define CAL_VERSION (2)
-#define WARM_VERSION (1)
-#define LKG_VERSION (1)
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-/*
- * This structure defines the header for one entry in a Calibration or
- * Persistent Archive Record.
- */
-typedef struct xrobjbitstreamhdr
-{
-    uint8_t xtraSize; /* header size plus any rounding padding */
-    char objectName[OBJNAME_MAX_SIZE];
-    uint8_t parmNum;
-    uint8_t padding[ALIGN_STRUCT];
-    uint16_t recType;
-    uint16_t startIndex;
-    uint32_t recSize;
-#define LKG_STD_PARM 0x01    /* lkg from Parameters */
-#define WARM_STD_PARM 0x11   /* warm from Parameters */
-#define WARM_PIC_PARM 0x12   /* warm from PicData setpoint */
-#define WARM_DAC_PARM 0x13   /* warm from DacData setpoint */
-#define WARM_ARY_PARM 0x14   /* warm from array */
-#define WARM_STD_PARM16 0x15 /* warm from 16-bit parameter */
-} xrobjbitstreamhdr;
-
-typedef struct archivehdr
-{
-    uint32_t size;       /* count of bytes following header */
-    uint32_t crc32;      /* CRC protects data following */
-    uint32_t version;    /* standard form (modified from older 16 bits) */
-    uint32_t generation; /* generation for archives */
-} archivehdr_t;
-#define PERSISTENT_ARCHIVE_HEADER_SIZE (sizeof(archivehdr_t) / sizeof(uint8_t))
-
 #define BACKUP_BREG0 (0x40271000)
 #define BACKUP_BREG1 (0x40271004)
 typedef enum
@@ -107,12 +42,4 @@ typedef union boot_info {
         uint32_t cb_count : 14U;
     } BITS;
 } boot_info_t;
-
-#define DEFAULTNEWRECORD "NRC"
-#define DEFAULTENDRECORD "END"
-#define CONTINUEEXISTINGRECORD "ERC"
-#define SIZE_OF(s, m) (sizeof(((s *)0)->m))
-#define XROBJ_BITSTREAM_HDR_NOOBJNAME_SIZE                                     \
-    (((sizeof(xrobjbitstreamhdr)) / (sizeof(uint8_t))) -                       \
-     (SIZE_OF(xrobjbitstreamhdr, objectName)))
 #endif /* XROBJ_HDR_DEFS_H_ */
